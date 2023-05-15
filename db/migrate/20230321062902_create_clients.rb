1181ecaddb4f532c9b081c6a55f33fc4271c62af
tanwarraja99 <rajatawar9399@gmail.com>
2023-05-15 18:42:30 +0530

    exampale

---
+++ db/migrate/20230321062902_create_clients.rb
@@ -0,0 +1,12 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :name
+      t.string :address
+      t.string :company
+      t.string :phone_number
+
+      t.timestamps
+    end
+  end
+end
